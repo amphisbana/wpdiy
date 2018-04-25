891726c94670fb10a96b2f68d64a4b85b45d1976
wangshaojie <1032335489@qq.com>
2018-04-25 19:15:55 +0800

    初始导入

---
+++ src/main/java/wang/sj/wpdiy/WpdiyApplication.java
@@ -0,0 +1,12 @@
+package wang.sj.wpdiy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WpdiyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WpdiyApplication.class, args);
+	}
+}
---
+++ src/test/java/wang/sj/wpdiy/WpdiyApplicationTests.java
@@ -0,0 +1,16 @@
+package wang.sj.wpdiy;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WpdiyApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
